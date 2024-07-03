cc4f890c9eb89cc83dd252ec18d97a50e3148bfd
Nathan Luu <nluu0122@gmail.com>
2024-07-03 10:54:37 -0400

    decks created endpoint

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,11 +5,6 @@ import { isLoggedIn } from "../index.js";
 const prisma = new PrismaClient();
 const router = Router();
 
-router.get("/decks", async (req, res) => {
-  const allDecks = await prisma.deck.findMany();
-  res.json(allDecks);
-});
-
 router.get("/decks/:deckId", isLoggedIn, async (req, res) => {
   const { deckId } = req.params;
 
@@ -21,9 +16,9 @@ router.get("/decks/:deckId", isLoggedIn, async (req, res) => {
         cards: true,
         FavoriteDeck: {
           where: {
-            userId: req.session.user.id
-          }
-        }
+            userId: req.session.user.id,
+          },
+        },
       },
     });
 
@@ -38,22 +33,31 @@ router.get("/decks/:deckId", isLoggedIn, async (req, res) => {
   }
 });
 
-router.get("users/:userId/decks", async (req, res) => {
-  const userId = parseInt(req.params.userId);
+router.get("/users/:userId/decks/created", isLoggedIn, async (req, res) => {
+  const { userId } = req.params;
 
-  try {
-    const userDecks = await prisma.deck.findMany({
-      where: {
-        userId: userId,
-      },
+  const currentUser = req.session.user;
 
-    });
-    res.json(userDecks);
-  } catch (error) {
-    console.error("Error fetching user decks:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  } finally {
-    await prisma.$disconnect();
+  if (currentUser.id !== userId) {
+    res.sendStatus(401);
+  } else {
+    try {
+      const decks = await prisma.deck.findMany({
+        where: {
+          userId: currentUser.id,
+        },
+        include: {
+          user: true,
+        },
+      });
+      if (!decks) {
+        return res.status(200).json([]);
+      }
+      res.json(decks);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Server error" });
+    }
   }
 });
 
@@ -64,8 +68,7 @@ router.get("/users/:userId/decks/favorites", isLoggedIn, async (req, res) => {
 
   if (currentUser.id !== userId) {
     res.sendStatus(401);
-  } 
-  else {
+  } else {
     try {
       const favoriteDecks = await prisma.favoriteDeck.findMany({
         where: {
@@ -73,14 +76,14 @@ router.get("/users/:userId/decks/favorites", isLoggedIn, async (req, res) => {
         },
         include: {
           deck: true,
-        }
+        },
       });
       if (!favoriteDecks) {
         return res.status(200).json([]);
       }
       res.json(favoriteDecks);
     } catch (error) {
-      console.log(error)
+      console.log(error);
       res.status(500).json({ message: "Server error" });
     }
   }

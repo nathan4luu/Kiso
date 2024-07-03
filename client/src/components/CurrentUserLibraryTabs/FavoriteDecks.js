2fad3cd1e93b35b0289fc65a8e601f486a1ed0f7
Nathan Luu <nluu0122@gmail.com>
2024-07-03 10:57:13 -0400

    fixed endpoint route

--- client/src/components/CurrentUserLibraryTabs/FavoriteDecks.js
+++ client/src/components/CurrentUserLibraryTabs/FavoriteDecks.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { CalendarSearch } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function ({ currentUserId }) {
+export default function FavoriteDecks({ currentUserId }) {
   const [favoriteDecks, setFavoriteDecks] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -12,7 +13,8 @@ export default function ({ currentUserId }) {
     const fetchFavoriteDecks = async () => {
       try {
         const response = await axios.get(
-          `/api/users/${currentUserId}/decks/favorites`
+          `http://localhost:4040/api/users/${currentUserId}/decks/favorites`,
+          { withCredentials: true }
         );
         setFavoriteDecks(response.data);
         setLoading(false);
@@ -28,16 +30,16 @@ export default function ({ currentUserId }) {
   }, []);
   if (!loading)
     return (
-      <>
+      <div className>
         {favoriteDecks.length === 0 ? (
           <p>Favorites? What favorites? You haven't picked any yet!</p>
         ) : (
           <div>
             {favoriteDecks.map((favorite, index) => (
-              <div key={index}>{favorite.deck.title}</div>
+              <div key={index} >{favorite.deck.title}</div>
             ))}
           </div>
         )}
-      </>
+      </div>
     );
 }

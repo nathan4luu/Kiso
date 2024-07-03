578f9cf3e33b76b168e0501fde75ac0e552b7d2b
Nathan Luu <nluu0122@gmail.com>
2024-07-03 18:42:51 -0400

    removed random space

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <div className="border-solid rounded-3xl px-4 py-1">
       <Link to="/dashboard"
-        className="flex items-center  font-shojumaru space-x-3 rtl:space-x-reverse text-3xl font-bold text-white whitespace-nowrap dark:text-[#6B46C1] select-none"
+        className="flex items-center font-shojumaru space-x-3 rtl:space-x-reverse text-3xl font-bold text-white whitespace-nowrap dark:text-[#6B46C1] select-none"
       >
         <h1 className=" self-center hidden md:block">
           Kiso

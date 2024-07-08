9732a6500c9585080d1d5bad2a8d6ceb04da74c9
Nathan Luu <nluu0122@gmail.com>
2024-07-08 19:42:31 -0400

    removed flex-wrap for responsiveness for smaller screens

--- client/src/components/CurrentUserLibrary.js
+++ client/src/components/CurrentUserLibrary.js
@@ -17,7 +17,7 @@ export default function CurrentUserLibrary() {
     <div>
       <h1 className="text-3xl font-medium py-6">Your library</h1>
       <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
-        <ul className="flex flex-wrap -mb-px">
+        <ul className="flex -mb-px">
           {tabChoices.map((tab, index) => (
             <li key={tab} className="me-2">
               <Link

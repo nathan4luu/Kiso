459631ee1d1b698a3c4b3f8089917b6d3e26bf67
Nathan Luu <nluu0122@gmail.com>
2024-07-09 13:25:48 -0400

    added time ago export and implementation

--- client/src/components/CurrentUserLibraryTabs/FavoriteDecks.js
+++ client/src/components/CurrentUserLibraryTabs/FavoriteDecks.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { CalendarSearch } from "lucide-react";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { getTimeAgo } from "./YourDecks";
 
 export default function FavoriteDecks({ currentUserId }) {
   const [favoriteDecks, setFavoriteDecks] = useState([]);
@@ -30,13 +31,28 @@ export default function FavoriteDecks({ currentUserId }) {
   }, []);
   if (!loading)
     return (
-      <div className>
+        <div>
         {favoriteDecks.length === 0 ? (
           <p>Favorites? What favorites? You haven't picked any yet!</p>
         ) : (
           <div>
             {favoriteDecks.map((favorite, index) => (
-              <div key={index} >{favorite.deck.title}</div>
+              <div
+                key={index}
+                className=" rounded-lg border-r-4 border-b-4 border-gray-100 bg-white transition-colors transition-shadow duration-300 hover:bg-gray-100 hover:border-r-4 hover:border-b-4 hover:border-[#6B46C1] hover:shadow-xl hover:shadow-[#ebe6f5]"
+              >
+                <Link to={`/decks/${favorite.deck.id}`} className="block p-4">
+                  <div className="gap-2 items-center ">
+                    <div className="inline-block items-center rounded-full bg-gray-200 px-2 py-1 text-xs font-semibold">
+                      {favorite.deck.termCount} terms
+                    </div>
+                    <div className="text-2xl font-semibold line-clamp-1 pt-1">
+                      {favorite.deck.title}
+                    </div>
+                    <div className="italic text-s text-slate-500">Favorited {getTimeAgo(favorite.favoritedAt)}</div>
+                  </div>
+                </Link>
+              </div>
             ))}
           </div>
         )}
--- client/src/components/CurrentUserLibraryTabs/YourDecks.js
+++ client/src/components/CurrentUserLibraryTabs/YourDecks.js
@@ -3,18 +3,18 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { formatDate, formatDistanceToNow } from "date-fns";
 
-export default function YourDecks({ currentUserId }) {
-  const [yourDecks, setYourDecks] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const navigate = useNavigate();
-
-  const getTimeAgo = (timestamp) => {
+export const getTimeAgo = (timestamp) => {
     const formattedTime = formatDistanceToNow(new Date(timestamp), {
       addSuffix: true,
     });
     return formattedTime.replace("about", "");
   };
+
+export default function YourDecks({ currentUserId }) {
+  const [yourDecks, setYourDecks] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchYourDecks = async () => {
@@ -45,17 +45,17 @@ export default function YourDecks({ currentUserId }) {
             {yourDecks.map((deck, index) => (
               <div
                 key={index}
-                className=" rounded-lg border-r-4 border-b-4 border-gray-100 bg-gray-50 transition-colors transition-shadow duration-300 hover:bg-gray-100 hover:border-r-4 hover:border-b-4 hover:border-[#6B46C1] hover:shadow-xl"
+                className=" rounded-lg border-r-4 border-b-4 border-gray-100 bg-white transition-colors transition-shadow duration-300 hover:bg-gray-100 hover:border-r-4 hover:border-b-4 hover:border-[#6B46C1] hover:shadow-xl hover:shadow-[#ebe6f5]"
               >
                 <Link to={`/decks/${deck.id}`} className="block p-4">
                   <div className="gap-2 items-center ">
                     <div className="inline-block items-center rounded-full bg-gray-200 px-2 py-1 text-xs font-semibold">
                       {deck.termCount} terms
                     </div>
-                    <h1 className="text-2xl font-semibold line-clamp-1 pt-1">
+                    <div className="text-2xl font-semibold line-clamp-1 pt-1">
                       {deck.title}
-                    </h1>
-                    <p className="italic text-s text-slate-500">Edited {getTimeAgo(deck.editedAt)}</p>
+                    </div>
+                    <div className="italic text-s text-slate-500">Edited {getTimeAgo(deck.editedAt)}</div>
                   </div>
                 </Link>
               </div>

52ddf2e841f5fce2e3c782437511e8c6aab8de8c
Nathan Luu <nluu0122@gmail.com>
2024-07-21 10:57:41 -0400

    added logo and longer display time to logout loading page

--- client/src/pages/Loading.js
+++ client/src/pages/Loading.js
@@ -9,11 +9,11 @@ export default function Loading() {
   const messages = [
     "Logging out... Time to let your brain absorb all that knowledge!",
     "What a great study session! An A+ is in your future!",
-    "You're leaving? The flashcards are gossiping about you right now.",
+    "You're leaving? The flashcards are gossiping about you right now...",
     "Logging out... Even geniuses need a moment to reboot.",
     "Sheesh... after all that studying maybe I need to take a break.",
     "Till next time... Practice makes... well, slightly less terrible, right?",
-    ""
+    "Logging out... Are you ditching us for cat videos again?",
   ];
 
   const getRandomMessage = () => {
@@ -28,19 +28,18 @@ export default function Loading() {
       logoutMutation.mutate();
       setTimeout(() => {
         navigate("/");
-      }, 1500);
+      }, 2000);
     };
 
     logoutAndNavigate();
   }, [logoutMutation, navigate]);
 
   return (
-    <div className="w-screen h-screen">
-      <div className="flex h-full w-full justify-center items-center">
-        <div className="space-y-4">
-          <LoadingSpinner />
-          <p>{message}</p>
-        </div>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="space-y-4 text-center">
+        <LoadingSpinner />
+        <p>{message}</p>
+        <p className="font-shojumaru text-purple-800">Kiso</p>
       </div>
     </div>
   );

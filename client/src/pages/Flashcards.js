e7b72fb284831694e20d19f56dfdea29b3e64641
Nathan Luu <nluu0122@gmail.com>
2024-07-21 10:59:29 -0400

    added back arrow for flashcards page

--- client/src/pages/Flashcards.js
+++ client/src/pages/Flashcards.js
@@ -1,7 +1,8 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import FlashcardCarousel from "../components/FlashcardCarousel";
 import { useDeck } from "../api/deck";
 import LoadingSpinner from "../components/ui/LoadingSpinner";
+import { ArrowLeft } from "lucide-react";
 
 export default function Flashcards() {
   const { deckId } = useParams();
@@ -13,10 +14,17 @@ export default function Flashcards() {
   }
 
   return (
-    <div className="flex w-full h-full items-center justify-center">
+    <div className="flex relative w-full h-full items-center justify-center">
       <div className="flex justify-center w-full">
         <FlashcardCarousel cards={deck.data.cards} />
       </div>
+      <Link
+        to={`/decks/${deckId}`}
+        className="flex items-center p-2 mt-4 space-x-2 absolute left-0 top-0 font-semibold text-xl hover:text-purple-main hover:underline"
+      >
+        <ArrowLeft />
+        <div>Back to {deck.data.title}</div>
+      </Link>
     </div>
   );
 }

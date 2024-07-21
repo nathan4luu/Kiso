fc0c12fb366c9043c513e3de63897088b5a95a74
Nathan Luu <nluu0122@gmail.com>
2024-07-21 10:58:01 -0400

    removed blue outline in dark mode for current user tab

--- client/src/components/CurrentUserLibrary.js
+++ client/src/components/CurrentUserLibrary.js
@@ -14,7 +14,7 @@ export default function CurrentUserLibrary() {
   };
 
   return (
-    <div>
+    <div className="">
       <h1 className="text-3xl font-medium py-6">Your library</h1>
       <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
         <ul className="flex -mb-px">
@@ -25,7 +25,7 @@ export default function CurrentUserLibrary() {
                 onClick={() => handleClick(tab)}
                 className={`inline-block p-4 border-b-2 rounded-t-lg ${
                   activeTab === tab
-                    ? "text-[#6B46C1] border-[#6B46C1] dark:text-blue-500 dark:border-blue-500"
+                    ? "text-[#6B46C1] border-[#6B46C1]"
                     : "border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
                 }`}
                 aria-current={activeTab === tab ? "page" : undefined}

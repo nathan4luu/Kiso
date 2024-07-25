97ad68eece91091541b677c00149de89b4416f23
Nathan Luu <nluu0122@gmail.com>
2024-07-25 19:04:33 -0400

    fixed header alignment

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -17,32 +17,21 @@ export default function Header() {
     <>
       <nav className="mx-auto bg-[#6B46C1] w-screen dark:bg-gray-900">
         <div className="max-w-full flex items-center justify-items-center justify-center px-1 py-2">
-          <div className="flex items-center flex-1">
+          <div className="flex items-center flex-1 items-center">
             <Logo />
           </div>
-          <div className="flex-1 px-4 py-1">
-            <div
-              className="justify-end w-full  items-center md:block md:w-auto ml-auto justify-end self-center"
-              id="navbar-default"
+          <div className="flex-1 px-4 py-1 items-center flex justify-end">
+            <Link
+              className="inline-block py-2 bg-white rounded-lg hover:bg-gray-200 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              onClick={() => document.getElementById("myModal").showModal()}
             >
-              <ul className="font-medium flex justify-end items-center text-white md:p-0 mt-4 rounded-lg bg-transparent md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:border-gray-700">
-                <li>
-                  <Link
-                    className="block py-2 bg-white rounded-lg hover:bg-gray-200 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                    onClick={() =>
-                      document.getElementById("myModal").showModal()
-                    }
-                  >
-                    <div className="flex gap-1 line-clamp-1 px-4 py-2 text-[#6B46C1]">
-                      <LogIn />
-                      Log In
-                    </div>
-                  </Link>
+              <div className="flex gap-1 line-clamp-1 px-4 py-2 text-[#6B46C1] justify-center">
+                <LogIn />
+                Log In
+              </div>
+            </Link>
 
-                  <LoginModal />
-                </li>
-              </ul>
-            </div>
+            <LoginModal />
           </div>
         </div>
       </nav>

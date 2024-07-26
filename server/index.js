a5d5347d2d0822c660f2c14c71167ed873f7f4ab
Nathan Luu <nluu0122@gmail.com>
2024-07-26 10:44:08 -0400

    added cookie expiration

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      maxAge: 24 * 60 * 60 * 1000, //one week
+      maxAge: 24 * 60 * 60 * 1000, //one day
     }
   })
 );

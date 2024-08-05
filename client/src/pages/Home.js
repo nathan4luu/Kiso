7f796f4daf03c01625607c9c0e8d2a22204f7e73
Nathan Luu <nluu0122@gmail.com>
2024-08-04 22:41:24 -0400

    changes to footer and landing page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,8 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { useUser } from "../api/user";
 import LoadingSpinner from "../components/ui/LoadingSpinner";
+import LoginModal from "../components/LoginModal";
 
 export default function Home() {
   const navigate = useNavigate();
@@ -16,11 +17,28 @@ export default function Home() {
   }
   if (user.data === null) {
     return (
-      <main>
-        <h1>
-          <b>Homepage</b>
-        </h1>
-        <p>This is EduCards</p>
+      <main className="flex h-full items-center justify-center">
+        <div>
+          <div className="flex text-7xl">
+            <div className="mr-3 font-bold">This is</div>
+            <p className="font-shojumaru text-purple-800">Kiso</p>
+          </div>
+          <div className="flex justify-center text-xl mt-4 font-semibold text-gray-500">
+            Limitless learning. Mastery made easy.
+          </div>
+          <div className="flex justify-center mt-2">
+            <Link
+              className="inline-block py-2 bg-purple-secondary rounded-lg hover:bg-purple-main md:p-0"
+              onClick={() => document.getElementById("myModal").showModal()}
+            >
+              <div className="flex gap-1 line-clamp-1 px-4 py-2 text-[#6B46C1] hover:text-white justify-center">
+                Get Started
+              </div>
+            </Link>
+          </div>
+
+          <LoginModal />
+        </div>
       </main>
     );
   }
